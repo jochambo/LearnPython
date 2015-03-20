55f163f51f5e053a51a1b7095a64f1beda0c208e
Joey Chamberlin <joey.chamberlin@gmail.com>
2015-03-20 11:02:03 -0500

    Added floating points.

--- ex3.py
+++ ex3.py
@@ -5,7 +5,7 @@ print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 print "Is it true that 3 + 2 < 5 - 7?"
 print 3 + 2 < 5 - 7
